2f82298747a6b52cddfc777e85e60bf1644bf9db
Mitkumar Patel <mzp5729@psu.edu>
2023-10-04 14:55:40 -0400

    Moved Weekly Update 1 and 2

--- app/src/main/java/com/example/budgetr/Homescreen.kt
+++ app/src/main/java/com/example/budgetr/Homescreen.kt
@@ -22,7 +22,7 @@ class Homescreen : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting4("Android")
+                    Greeting4("Budgetr")
                 }
             }
         }
